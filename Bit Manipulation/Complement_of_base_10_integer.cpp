40dfc07c5e491057b0db50c2231b283bdd689494
bhoomi-meena17 <bhoomimeena7201>
2026-06-26 22:52:07 +0530

    Added Complement of base 10 integer solution

---
+++ Bit Manipulation/Complement_of_base_10_integer.cpp
@@ -0,0 +1,28 @@
+"""
+LeetCode #5
+
+Problem: Complement of Base 10 Integer
+
+Difficulty: Easy
+
+Topics:
+- Bit Manipulation
+
+"""
+
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int m = n;
+        int mask = 0;
+        if (n==0){
+            return 1;
+        }
+        while (m!=0 ){
+            mask = (mask<<1)|1;
+            m = m>>1;
+        }
+        int ans = (~n) & mask;
+        return ans;
+    }
+};
